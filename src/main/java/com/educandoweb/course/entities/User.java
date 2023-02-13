73200a3d617c43d0d333d9a990fe8362e4331099
Gustavo Pinheiro de Oliveira <mrpine19@gmail.com>
2023-02-13 12:16:12 -0300

    Order, Instant, ISO 8601 - PART 2

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -25,6 +27,7 @@ public class User implements Serializable{
 	private String phone;
 	private String password;
 	
+	@JsonIgnore //essa anotação faz com que não ocorra um looping entre user e order
 	@OneToMany(mappedBy = "client")
 	private List<Order> orders = new ArrayList<>();
 	
